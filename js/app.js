70012bac4f5c7527b5faade4f5186875b67f8b54
Natanael Borges de Oliveira <naeelbass@gmail.com>
2024-04-19 19:39:48 -0300

    console quantidade alugada

--- js/app.js
+++ js/app.js
@@ -1,17 +1,37 @@
+let jogosAlugados = 0
+
+function contarEExibirJogosAlugados() {
+    console.log(`Total de jogos alugados: ${jogosAlugados}`);
+}
+
 function alterarStatus(id) {
     let gameSelecionado = document.getElementById(`game-${id}`);
     let imagem = gameSelecionado.querySelector('.dashboard__item__img');
     let botao = gameSelecionado.querySelector('.dashboard__item__button');
+    let nomeJogo = gameSelecionado.querySelector('.dashboard__item__name')
     
     if (imagem.classList.contains('dashboard__item__img--rented')) {
-        imagem.classList.remove('dashboard__item__img--rented');
-        botao.textContent = 'Alugar';
-        botao.classList.remove('dashboard__item__button--return');
-        alert(`O jogo foi devolvido`);
+        
+        if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}?`)) {
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.textContent = 'Alugar';
+            botao.classList.remove('dashboard__item__button--return');
+            jogosAlugados--;
 
-    } else {
+        } 
+    } else {  
         imagem.classList.add('dashboard__item__img--rented');
         botao.textContent = 'Devolver';
         botao.classList.add('dashboard__item__button--return');
-    }
-}   
+        console.log()
+        jogosAlugados++;
+            }
+            
+            contarEExibirJogosAlugados();
+}      
+
+// Inicializa a contagem considerando que os jogos já começam alugados
+document.addEventListener('DOMContentLoaded', function() {
+    jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    contarEExibirJogosAlugados();
+});
